a6d1438dd3f1a93186a2e8a838bdde1d9a85316b
Kalinina Oksana <ksan4ik92@gmail.com>
2023-08-25 11:37:58 +0300

    feat: view points in game area

--- src/components/GameArea/GameArea.tsx
+++ src/components/GameArea/GameArea.tsx
@@ -1,29 +1,16 @@
 import './GameArea.css'
 function GameArea() {
+    const arrQuestionsPoints = [["деятельность", 100, 200, 300, 400], ["география", 100, 200, 300, 400],["цифры и даты", 100, 200, 300, 400],["работа у нас", 100, 200, 300, 400],]
 
   return (
     <>
             <div className="main__game game">
-            <div className="game__category">Деятельность</div>
-            <div className="game__item game__item_disable">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__category">География</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__category">цифры и даты</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__category">работа у нас</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
-            <div className="game__item">100</div>
+                {arrQuestionsPoints.map((arr, arrIndex)=>{
+                    return arr.map((item, index)=>{
+                        return index === 0 ? <div className="game__category" key={index}>{item}</div> :
+                        <div className={"game__item" + (item === 0 ? " disable" : "")} key={arrIndex + " " + index}>{index*100}</div>
+                    })
+                })}
         </div>
     </>
   )
