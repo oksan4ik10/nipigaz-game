618b5975771cdd223ff71109a759f7a1a1948d4d
Kalinina Oksana <ksan4ik92@gmail.com>
2023-08-29 00:01:56 +0300

    feat: add task20

--- src/components/Tasks/Task20/Task20.tsx
+++ src/components/Tasks/Task20/Task20.tsx
@@ -1,5 +1,5 @@
-import './Task20.css'
-import { FormEvent, useState } from 'react';
+import styles from "./Task20.module.css"
+import {  useState } from 'react';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
 interface IProps {
@@ -12,30 +12,38 @@ function Task20(props: IProps) {
     const dispatch = useAppDispatch();
     const {selectAnswer, checkClick} = props;
 
-    const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
-        const target = e.target as HTMLInputElement;
+    const [spanWidth, setSpanWidth] = useState("0%");
+    const [userAnswer, setUserAnswer] = useState("0");
+    
+    const changeInput = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        const el  = e.currentTarget;
 
-        selectAnswer(); //пользователь выбрал хотя бы один вариант
-
-        if(target.value === "dzen"){
+        setSpanWidth(el.value + "%");
+        setUserAnswer(el.value);
+        setChecked(false);
+        selectAnswer();
+        if(el.value === "50"){
             dispatch(setCheckAnswer("true"));
         } else {
             dispatch(setCheckAnswer("false"));
         }
-    
-
     }
-
   return (
     <>
-                <div className="task__info">
+                <div className={styles.task__info}>
                         <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Расположи полузнок на верной цифре</h4>
-                        <form onChange={clickFormTest}>
-                        <p><b>Какое у вас состояние разума?</b></p>
-                            <p><input name="dzen" type="radio" value="nedzen" defaultChecked={true} onChange={() => setChecked(!checked)}/> Не дзен</p>
-                            <p><input name="dzen" type="radio" value="dzen"/> Дзен</p>
-                            <p><input name="dzen" type="radio" value="pdzen"/> Полный дзен</p>
-                        </form> 
+                        <form className = {styles.form}>
+                            <div className={checked ? styles.numbers : styles.numbersActive}>
+                                <span style={!checked ? {"display": "none"} : {"display":"block"}}>0</span>
+                                <span style={checked ? {"display": "none"} : {"display":"block"}} className={styles.userAnswer}>{userAnswer}</span>
+                                <span style={!checked ? {"display": "none"} : {"display":"block"}}>100</span>
+                            </div>
+                            <div className={styles.progress}>
+                                <input type="range" className={styles.range} min="0" max="100" step="10" defaultValue="0" onChange={changeInput}/>
+                                <span style={{"width": spanWidth}}></span>
+                            </div>
+
+                        </form>
 
                 </div>
             </>
