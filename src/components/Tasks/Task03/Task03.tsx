035d7884aeb3e11f057e4ed397d318e577a74118
Kalinina Oksana <ksan4ik92@gmail.com>
2023-08-30 22:52:22 +0300

    feat: add events touch in task03

--- src/components/Tasks/Task03/Task03.tsx
+++ src/components/Tasks/Task03/Task03.tsx
@@ -3,6 +3,7 @@ import styles from './Task03.module.css';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
 import { OpacityTask } from '../../../utils/OpacityTask/OpacityTask';
+import { TouchEvent } from 'react';
 
 interface IProps {
     selectAnswer: () => void;
@@ -12,7 +13,7 @@ interface IProps {
 function Task03(props: IProps) {
     const dispatch = useAppDispatch();
     const {selectAnswer, checkClick} = props;
-    const { register, getValues } = useForm();
+    const { register, getValues, setValue } = useForm();
 
     const clickFormTest = ()=>{
         const {name1} = getValues();
@@ -22,6 +23,20 @@ function Task03(props: IProps) {
         } else {
             dispatch(setCheckAnswer("false"));
         }
+    } 
+
+    let userCheck: HTMLElement | null = null; 
+    
+    const onTouchStart = (e: TouchEvent) => {
+        userCheck = e.target as HTMLElement;
+    }
+    const onTouchEnd =  (e: TouchEvent) => {
+        if(!userCheck) return;
+        const target = e.target as HTMLElement;
+        if(target === userCheck) {
+            setValue('name1', target.getAttribute("data-value"));
+            clickFormTest();
+        }
     }
 
   return (
@@ -29,22 +44,22 @@ function Task03(props: IProps) {
                         <div className={styles.taskInfo}>
                         {checkClick && <OpacityTask/>}  
                         <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Вычеркни выбранный ответ</h4>
-                        <form className={styles.form} onChange={(clickFormTest)}>
-                            <label className={styles.label}>
+                        <form className={styles.form} onChange={(clickFormTest)} onTouchStart={onTouchStart} onTouchEnd={onTouchEnd}>
+                            <label className={styles.label} >
                                 <input type="radio" className={styles.input} value={1} {...register("name1")}/>
-                                <span>Водоснабжение и канализация</span>
+                                <span data-value="1">Водоснабжение и канализация</span> 
                             </label>
                             <label className={styles.label}>
                                 <input type="radio" className={styles.input} value={2} {...register("name1")}/>
-                                <span>Машиностроение и робототехника</span>
+                                <span data-value="2">Машиностроение и робототехника</span>
                             </label>
                             <label className={styles.label}>
                                 <input type="radio" className={styles.input} value={3} {...register("name1")}/>
-                                <span>Генплан и транспорт</span>
+                                <span data-value="3">Генплан и транспорт</span>
                             </label>
                             <label className={styles.label}>
                                 <input type="radio" className={styles.input} value={4} {...register("name1")}/>
-                                <span>Связь, безопасность и телекоммуникации</span>
+                                <span data-value="4">Связь, безопасность и телекоммуникации</span>
                             </label>
 
                         </form>
