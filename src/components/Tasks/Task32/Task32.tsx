da9392177a1e6ec3957e35aa827a2368b8878cf0
Kalinina Oksana <ksan4ik92@gmail.com>
2023-08-30 14:31:45 +0300

    feat: task32

--- src/components/Tasks/Task32/Task32.tsx
+++ src/components/Tasks/Task32/Task32.tsx
@@ -1,44 +1,55 @@
-import './Task32.css'
-import { FormEvent, useState } from 'react';
+import { useForm} from 'react-hook-form';
+import styles from './Task32.module.css';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
+
 interface IProps {
     selectAnswer: () => void;
     checkClick: boolean;
 }
 
 function Task32(props: IProps) {
-    const [checked, setChecked] = useState(true);
     const dispatch = useAppDispatch();
     const {selectAnswer, checkClick} = props;
+    const { register, getValues } = useForm();
 
-    const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
-        const target = e.target as HTMLInputElement;
-
+    const clickFormTest = ()=>{
+        const {name1} = getValues();
         selectAnswer(); //пользователь выбрал хотя бы один вариант
-
-        if(target.value === "dzen"){
+        if(name1 === "2"){
             dispatch(setCheckAnswer("true"));
         } else {
             dispatch(setCheckAnswer("false"));
         }
-    
-
     }
 
   return (
     <>
-                <div className="task__info">
-                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Расположи полузнок на верной цифре</h4>
-                        <form onChange={clickFormTest}>
-                        <p><b>Какое у вас состояние разума?</b></p>
-                            <p><input name="dzen" type="radio" value="nedzen" defaultChecked={true} onChange={() => setChecked(!checked)}/> Не дзен</p>
-                            <p><input name="dzen" type="radio" value="dzen"/> Дзен</p>
-                            <p><input name="dzen" type="radio" value="pdzen"/> Полный дзен</p>
-                        </form> 
+                        <div className={styles.taskInfo}>
+                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Вычеркни выбранный ответ</h4>
+                        <form className={styles.form} onChange={(clickFormTest)}>
+                            <label className={styles.label}>
+                                <input type="radio" className={styles.input} value={1} {...register("name1")}/>
+                                <span>Юный инженер</span>
+                            </label>
+                            <label className={styles.label}>
+                                <input type="radio" className={styles.input} value={2} {...register("name1")}/>
+                                <span>Молодой специалист</span>
+                            </label>
+                            <label className={styles.label}>
+                                <input type="radio" className={styles.input} value={3} {...register("name1")}/>
+                                <span>Начинающий эксперт</span>
+                            </label>
+                            <label className={styles.label}>
+                                <input type="radio" className={styles.input} value={4} {...register("name1")}/>
+                                <span>Будущий профессионал</span>
+                            </label>
+
+                        </form>
+
 
                 </div>
-            </>
+    </>
   )
 }
 
