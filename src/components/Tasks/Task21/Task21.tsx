276f403de552dd6a58423ea32e4e765dc139577e
Kalinina Oksana <ksan4ik92@gmail.com>
2023-09-02 23:10:18 +0300

    fix: x and y position touch elem in task21:

--- src/components/Tasks/Task21/Task21.tsx
+++ src/components/Tasks/Task21/Task21.tsx
@@ -1,15 +1,11 @@
 import styles from './Task21.module.css'
-import { FormEvent, useState } from 'react';
+import { useRef, useState, useEffect} from 'react';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
 import { OpacityTask } from '../../../utils/OpacityTask/OpacityTask';
 interface IProps {
     selectAnswer: () => void;
     checkClick: boolean;
-}
-interface IAnswer {
-    value: string,
-    answer: boolean
 }
 
 function Task21(props: IProps) {
@@ -32,27 +28,24 @@ function Task21(props: IProps) {
 
     const [saveResult, setSaveResult] = useState(false);
 
-    const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
-        const target = e.target as HTMLInputElement;
-
-        selectAnswer(); //пользователь выбрал хотя бы один вариант
-
-        if(target.value === "dzen"){
-            dispatch(setCheckAnswer("true"));
-        } else {
-            dispatch(setCheckAnswer("false"));
+    const ref = useRef<HTMLDivElement>(null);
+    let stateX = 0, stateY = 0;
+    useEffect(()=> {
+        if(ref.current){
+          const data = ref.current.getBoundingClientRect();
+          stateX = data.left;
+          stateY = data.top;
+            
         }
-    
+        
+    })
 
-    }
     let targetDrag: HTMLElement| undefined;
     const dragStart = (e: React.TouchEvent<HTMLDivElement>) => {
-        const data = e.changedTouches[0];
-
-        
+        const data = e.changedTouches[0];     
         targetDrag = e.changedTouches[0].target as HTMLElement;
-        const y = data.pageY - 356/2 + 30 - (targetDrag.offsetHeight / 2);
-        const x = data.pageX - 157/2 - (targetDrag.offsetWidth / 2);
+        const y = data.pageY  - stateY - (targetDrag.offsetHeight / 2);
+        const x = data.pageX - stateX - (targetDrag.offsetWidth / 2);
         targetDrag.style.position = "absolute";
         targetDrag.style.left = x + "px";
         targetDrag.style.top = y + "px";
@@ -61,8 +54,8 @@ function Task21(props: IProps) {
     const dragMove = (e: React.TouchEvent<HTMLDivElement>) => {
         const data = e.changedTouches[0];
         if(targetDrag){
-            const y = data.pageY - 356/2 + 30 - (targetDrag.offsetHeight / 2);
-            const x = data.pageX - 157/2 - (targetDrag.offsetWidth / 2);
+            const y = data.pageY  - stateY - (targetDrag.offsetHeight / 2);
+            const x = data.pageX - stateX - (targetDrag.offsetWidth / 2);
             if( y > 14 && y < 112 )targetDrag.style.top = y  + "px";
             if (x > 0 && x < 270) targetDrag.style.left = x + "px";
             if(((y > 40) && (y < 80)) && ((x > 40) && (x < 227))){
@@ -104,7 +97,7 @@ function Task21(props: IProps) {
 
   return (
     <>
-                <div className={styles.taskInfo}>
+                <div className={styles.taskInfo} ref={ref}>
                         {checkClick && <OpacityTask/>}
                         <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Перетащи верное количество нулей в ячейку</h4>
                         <div className={styles.task}>
@@ -112,23 +105,24 @@ function Task21(props: IProps) {
                                 <span>
                                     1
                                 </span>
-                                <div className={styles.answer}>
+                                <div className={styles.answer} >
                                     <div className={styles.zero + " " + (answer.answer ? "" : styles.none) + " " + 
-                                ( checkClick ? saveResult ? styles.success : styles.danger:"")}>{answer.value}</div>
+                                ( checkClick ? saveResult ? styles.success : styles.danger:"")} 
+                                >{answer.value}</div>
                                 </div>
                                 <span>+</span>
                             </div>
                         </div>
                         <div className={styles.taskZeros}>
                             { arrAnswers.map((item, index)=>{
-                                return <div>
+                                return <div key={item.value}>
                                 <div 
                                 onTouchStart = {(e) => dragStart(e)}
                                 onTouchMove={(e) => dragMove(e)}
                                 onTouchEnd={(e) => dragEnd(e, index)}
                                 className={styles.zero + " " + (item.answer ? styles.none : "") + " " + (
                                     checkClick ? saveResult ? styles.success : styles.danger:""
-                                )}>{item.value}</div></div>
+                                )} >{item.value}</div></div>
                             })}
                         </div>
                         
