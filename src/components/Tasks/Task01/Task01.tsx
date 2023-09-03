c169d7f733eb804b2f3d627a976a8f1e24e4fc86
Kalinina Oksana <ksan4ik92@gmail.com>
2023-09-03 15:27:13 +0300

    feat: add task01

--- src/components/Tasks/Task01/Task01.tsx
+++ src/components/Tasks/Task01/Task01.tsx
@@ -1,41 +1,130 @@
-import './Task01.css'
-import { FormEvent, useState } from 'react';
+import styles from './Task01.module.css'
+import { useState, useRef, useEffect } from 'react';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
+import urlImg from "../../../assets/images/cart.png";
 interface IProps {
     selectAnswer: () => void;
     checkClick: boolean;
 }
 
 function Task01(props: IProps) {
-    const [checked, setChecked] = useState(true);
+
     const dispatch = useAppDispatch();
     const {selectAnswer, checkClick} = props;
 
-    const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
-        const target = e.target as HTMLInputElement;
-
-        selectAnswer(); //пользователь выбрал хотя бы один вариант
-
-        if(target.value === "dzen"){
-            dispatch(setCheckAnswer("true"));
-        } else {
-            dispatch(setCheckAnswer("false"));
+    const ref = useRef<HTMLDivElement>(null);
+    let stateX = 0, stateY = 0;
+    useEffect(()=> {
+        if(ref.current){
+          const data = ref.current.getBoundingClientRect();
+          stateX = data.left;
+          stateY = data.top;
+            
         }
-    
+        
+    })
 
+    const [arrAnswers, setArrAnswers] = useState([{
+        value: "EPC", check: false}, 
+        {value: "САПР", check: false}, {value: "UGC", check: false}, {value: "BIM", check: false }]);
+    const [answerIndex, setAnswerIndex] = useState("-1");
+
+    
+    let targetDrag: HTMLElement|undefined;
+    let saveX = "";
+
+    const dragStart = (e: React.TouchEvent<HTMLSpanElement>) =>{
+        const data = e.changedTouches[0]; 
+        targetDrag = e.changedTouches[0].target as HTMLElement;        
+        if(targetDrag.style.position ==="absolute") return;
+        const y = data.pageY  - stateY - (targetDrag.offsetHeight / 2);
+        const x = data.pageX - stateX - (targetDrag.offsetWidth / 2);
+        targetDrag.style.position = "absolute";
+        targetDrag.style.zIndex = "1";
+        targetDrag.style.left = x + "px";
+        targetDrag.style.top = y + "px";
     }
+
+    const dragMove = (e: React.TouchEvent<HTMLSpanElement>) =>{
+        const data = e.changedTouches[0];
+        
+        if(targetDrag){
+            if((targetDrag.style.top === "36px")) return;
+            const y = data.pageY  - stateY - (targetDrag.offsetHeight / 2);
+            const x = data.pageX - stateX - (targetDrag.offsetWidth / 2);
+            if( y > 14 && y < 112 )targetDrag.style.top = y  + "px";
+            if (x > 0 && x < 270) targetDrag.style.left = x + "px";
+            if(((y > 14) && (y < 140)) && ((x > 90) && (x < 170))){
+                const value = targetDrag.getAttribute("data-value");
+                if(value === "1") targetDrag.style.fontSize = "20px";
+
+                targetDrag.style.color = "#008C95";
+                targetDrag.style.top = 36  + "px";
+                saveX = stateX + (66 - targetDrag.offsetWidth)/2 + "px"; 
+                targetDrag.style.left = saveX;
+
+            } else{
+                targetDrag.style.color = "#fff";
+                targetDrag.style.fontSize = "25px";
+            }
+        
+        }
+    }
+    const dragEnd = (e: React.TouchEvent<HTMLSpanElement>) =>{
+        e.preventDefault();
+        if(targetDrag){
+            if((targetDrag.style.top !== "36px") && (targetDrag.style.left !== saveX)){
+                targetDrag.style.position = "static";
+                return;
+            }
+            const value = targetDrag.getAttribute("data-value");
+            if(value) {
+                setArrAnswers(arrAnswers.map((item, index)=> {
+                    if (index + "" === value) item.check = true;
+                    else item.check = false;
+                    return item
+                }))
+                if(value === "2"){
+                    dispatch(setCheckAnswer("true"));
+                } else {
+                    dispatch(setCheckAnswer("false"));
+                }
+                setAnswerIndex(value);
+            } 
+            selectAnswer();
+        }
+        
+        
+    }
+
 
   return (
     <>
-                <div className="task__info">
-                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Расположи полузнок на верной</h4>
-                        <form onChange={clickFormTest}>
-                        <p><b>Какое у вас состояние разума?</b></p>
-                            <p><input name="dzen" type="radio" value="nedzen" defaultChecked={true} onChange={() => setChecked(!checked)}/> Не дзен</p>
-                            <p><input name="dzen" type="radio" value="dzen"/> Дзен</p>
-                            <p><input name="dzen" type="radio" value="pdzen"/> Полный дзен</p>
-                        </form> 
+                <div className={styles.taskInfo}>
+                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Перетащи выбранный вариант ответа в корзину</h4>
+                        <div className={styles.task} ref={ref}>
+                            {arrAnswers.map((item, index) => 
+                            <div className={styles.taskAnswer} 
+
+                            key={index}>
+                                <span onTouchStart={(e) => dragStart(e)}
+                                onTouchMove = {(e) => dragMove(e)}
+                                
+                                onTouchEnd = {(e) => dragEnd(e)}
+                                data-value = {index}
+                                style={{
+                                    color: !item.check ? "#fff" : checkClick ? (answerIndex === "2") ? "#008C95" : "#C00000" : "#008C95", position: item.check ? "absolute" : "static"}} 
+                                >
+                                    {item.value}
+                                   
+                                </span>
+                            </div>
+                            )}
+                            <div className={styles.cart} >
+                                <img src={urlImg} alt="cart" />
+                            </div>
+                        </div>
 
                 </div>
             </>
