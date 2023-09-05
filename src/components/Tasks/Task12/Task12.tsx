d1a6cfd52c882ab954b7d1f739e57ff7d7c97f19
Kalinina Oksana <ksan4ik92@gmail.com>
2023-09-05 17:02:15 +0300

    feat: add styles for task12

--- src/components/Tasks/Task12/Task12.tsx
+++ src/components/Tasks/Task12/Task12.tsx
@@ -1,41 +1,80 @@
-import './Task12.css'
+import styles from "./Task12.module.css";
 import { FormEvent, useState } from 'react';
 import {useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
+import imgUrlSpan1 from  "../../../assets/images/numbers/1.svg";
+import imgUrlSpan2 from  "../../../assets/images/numbers/2.svg";
+import imgUrlSpan3 from  "../../../assets/images/numbers/3.svg";
+import imgUrlSpan4 from  "../../../assets/images/numbers/4.svg";
+import imgUrlSpan5 from  "../../../assets/images/numbers/5.svg";
+import imgUrlSpan6 from  "../../../assets/images/numbers/6.svg";
+import imgUrlSpan7 from  "../../../assets/images/numbers/7.svg";
+import imgUrlSpan8 from  "../../../assets/images/numbers/8.svg";
+import imgUrlSpan9 from  "../../../assets/images/numbers/9.svg";
+import imgUrlSpan10 from  "../../../assets/images/numbers/10.svg";
+import imgUrlSpan11 from  "../../../assets/images/numbers/11.svg";
+import imgUrlSpan12 from  "../../../assets/images/numbers/12.svg";
+import imgUrlHour from "../../../assets/images/numbers/hour.svg";
+import imgUrlMinute from "../../../assets/images/numbers/minute.svg";
 interface IProps {
     selectAnswer: () => void;
     checkClick: boolean;
 }
 
 function Task12(props: IProps) {
-    const [checked, setChecked] = useState(true);
+
     const dispatch = useAppDispatch();
     const {selectAnswer, checkClick} = props;
 
-    const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
-        const target = e.target as HTMLInputElement;
+    // const clickFormTest = (e: FormEvent<HTMLFormElement>)=>{
+    //     const target = e.target as HTMLInputElement;
 
-        selectAnswer(); //пользователь выбрал хотя бы один вариант
+    //     selectAnswer(); //пользователь выбрал хотя бы один вариант
 
-        if(target.value === "dzen"){
-            dispatch(setCheckAnswer("true"));
-        } else {
-            dispatch(setCheckAnswer("false"));
-        }
+    //     if(target.value === "dzen"){
+    //         dispatch(setCheckAnswer("true"));
+    //     } else {
+    //         dispatch(setCheckAnswer("false"));
+    //     }
     
 
-    }
+    // }
 
   return (
     <>
-                <div className="task__info">
-                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Расположи полузнок на верной цифре</h4>
-                        <form onChange={clickFormTest}>
-                        <p><b>Какое у вас состояние разума?</b></p>
-                            <p><input name="dzen" type="radio" value="nedzen" defaultChecked={true} onChange={() => setChecked(!checked)}/> Не дзен</p>
-                            <p><input name="dzen" type="radio" value="dzen"/> Дзен</p>
-                            <p><input name="dzen" type="radio" value="pdzen"/> Полный дзен</p>
-                        </form> 
+                <div className={styles.taskInfo}>
+                <div className={styles.clock}>
+                    <div className={styles.numbers}>
+                        <span>
+                            <img src={imgUrlSpan1} alt="1" />
+                        </span>
+                        <span><img src={imgUrlSpan2} alt="2" /></span>
+                        <span><img src={imgUrlSpan3} alt="3" /></span>
+                        <span><img src={imgUrlSpan4} alt="4" /></span>
+                        <span><img src={imgUrlSpan5} alt="5" /></span>
+                        <span><img src={imgUrlSpan6} alt="6" /></span>
+                        <span><img src={imgUrlSpan7} alt="7" /></span>
+                        <span><img src={imgUrlSpan8} alt="8" /></span>
+                        <span>
+                            <img src={imgUrlSpan9} alt="9" />
+                        </span>
+                        <span><img src={imgUrlSpan10} alt="10" /></span>
+                        <span><img src={imgUrlSpan11} alt="11" /></span>
+                        <span><img src={imgUrlSpan12} alt="12" /></span>
+                    </div>
+                    <div className={styles.arrows}>
+                        <div className={styles.hour}>
+                            <img src={imgUrlHour} alt="hour" />
+                        </div>
+                    </div>
+                    <div className={styles.minutes}>
+                        <img src={imgUrlMinute} alt="minute" />
+                    </div>
+
+                </div>
+
+                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Передвинь часовую стрелку в верное положение</h4>
+
 
                 </div>
             </>
