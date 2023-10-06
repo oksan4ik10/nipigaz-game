a4844a8ff5f89451a8942ebe102c0c9c13e0cbf5
Kalinina Oksana <ksan4ik92@gmail.com>
2023-10-06 13:55:42 +0300

    fix: v 5.0

--- src/components/Tasks/TaskEnd/TaskEnd.tsx
+++ src/components/Tasks/TaskEnd/TaskEnd.tsx
@@ -1,31 +1,31 @@
 import { useAppSelector } from '../../../store/store';
 import styles from "./TaskEnd.module.css";
-import imgUrlCode from "../../../assets/images/qr-code.svg"
+import imgUrlCode from "../../../assets/images/qr-code.png"
 
 
 
 function TaskEnd() {
-const points = useAppSelector((state)=>state.points).points;
+  const points = useAppSelector((state) => state.points).points;
   return (
     <>
 
-                <div className={styles.task__info}>
-                  
-                        <h4 className={styles.subtitels}>Баланс </h4>
-                        <p className={styles.points}>{points}</p>
+      <div className={styles.task__info}>
 
-                </div>
-                <div className={styles.contact}>
-                  <div className={styles.contactInfo}>
-                    <img src={imgUrlCode} alt="qr-code" />
-                    <p className={styles.contactText}>
-                    Хочешь у нас работать?<br/>Присоединяйся к команде!
-                    </p>
-                    <a href="http://career.nipigas.ru/studentam/programma-molodoy-spetsialist/ " className={styles.btnEnd}>откликнуться</a>
-                  </div>
+        <h4 className={styles.subtitels}>Баланс </h4>
+        <p className={styles.points}>{points}</p>
 
-                </div>
-            </>
+      </div>
+      <div className={styles.contact}>
+        <div className={styles.contactInfo}>
+          <img src={imgUrlCode} alt="qr-code" />
+          <p className={styles.contactText}>
+            Хочешь у нас работать?<br />Присоединяйся к команде!
+          </p>
+          <a href="http://career.nipigas.ru/studentam/programma-molodoy-spetsialist/ " className={styles.btnEnd}>откликнуться</a>
+        </div>
+
+      </div>
+    </>
   )
 }
 
