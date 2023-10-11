348499bb5a283c8e813c82c9d09af42d9fde06cf
Kalinina Oksana <ksan4ik92@gmail.com>
2023-10-11 15:14:51 +0300

    fix: 10-2

--- src/components/Tasks/Task32/Task32.tsx
+++ src/components/Tasks/Task32/Task32.tsx
@@ -1,153 +1,80 @@
-import { useForm} from 'react-hook-form';
-import { TouchEvent, MouseEvent, useRef} from 'react';
+import { useForm } from 'react-hook-form';
+import { MouseEvent } from 'react';
 import styles from './Task32.module.css';
-import {useAppDispatch } from '../../../store/store';
+import { useAppDispatch } from '../../../store/store';
 import { setCheckAnswer } from '../../../store/reducers/checkAnswerReducer';
 
-import { OpacityTask} from '../../../utils/OpacityTask/OpacityTask';
+import { OpacityTask } from '../../../utils/OpacityTask/OpacityTask';
 import { addAnswer } from "../../../store/reducers/answersReducer";
 import { IAnswerUser } from '../../../store/reducers/answersReducer';
 import { IProps } from '../types';
 
 function Task32(props: IProps) {
     const dispatch = useAppDispatch();
-    const {selectAnswer, checkClick, active} = props;
-    const { register, setValue } = useForm();
-
-    let targetDrag: HTMLElement| null;
-    let proc: number;
+    const { selectAnswer, checkClick, active } = props;
+    const { register } = useForm();
 
     const clickForm = (e: MouseEvent) => {
-        e.preventDefault();
-    }
-    const startClick = useRef(false);
-
-    const dragStart = (e: TouchEvent) =>{
-        const data = e.changedTouches[0].target as HTMLElement;
-        start(data);
-    }
-    const mouseStart = (e: MouseEvent) => {
         const target = e.target as HTMLElement;
-        startClick.current = true;
-        start(target);
-    }
-    const start = (target: HTMLElement) => {
-        proc = 0;
-        targetDrag = target.closest("label");
-        if (targetDrag) targetDrag.style.setProperty("--var-width", `0%`);
-    }
-    const dragMove = () =>{
-        move();
-    }
-    const mouseMove = () => {
-        if(!startClick.current) return;
-        move();        
-    }
-    const move = () => {
-        if(targetDrag){
-            if(proc === 0) if(targetDrag) setValue("name1", targetDrag.id);
-            proc += 3;
-            if(proc > 100) proc = 100;
-            targetDrag.style.setProperty("--var-width", `${proc}%`);
-        }
-    }
-
-    const dragEnd = () =>{
-        end();
-    }
-    const end = () => {
-        if(proc < 5) {
-            setValue("name1", "");
-            if (targetDrag) targetDrag.style.setProperty("--var-width", `0%`);
-            selectAnswer(false);
-            return;
-        }
-        if(targetDrag){
-            selectAnswer(true);
-            targetDrag.style.setProperty("--var-width", `100%`);
-            const answerUser: IAnswerUser = {
-                arrAnswer: active,
-                answerInfo: {
-                    answer: targetDrag.id ,
-                    correct: false
+        if (target) {
+            const label = target.closest("label");
+            if (label) {
+                label.style.setProperty("--var-width", `100%`);
+                selectAnswer(true);
+                const answerUser: IAnswerUser = {
+                    arrAnswer: active,
+                    answerInfo: {
+                        answer: label.id,
+                        correct: false
+                    }
+                }
+                if (label.id === "2") {
+                    dispatch(setCheckAnswer("true"));
+                    answerUser.answerInfo.correct = true;
+                    dispatch(addAnswer(answerUser))
+                } else {
+                    dispatch(setCheckAnswer("false"));
+                    dispatch(addAnswer(answerUser))
                 }
             }
-            if(targetDrag.id === "2"){
-                dispatch(setCheckAnswer("true"));
-                answerUser.answerInfo.correct = true;
-                dispatch(addAnswer(answerUser))
-            } else {
-                dispatch(setCheckAnswer("false"));
-                dispatch(addAnswer(answerUser))
-            }
+
         }
+
+
     }
 
 
-    const mouseUp = () => {
-        startClick.current = false;
-        end();
-        
-    }
+    return (
+        <>
+            <div className={styles.taskInfo}>
+                {checkClick && <OpacityTask />}
+                <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Подчеркни верный ответ, кликнув на него</h4>
+                <form className={styles.form} onClick={
+                    (e) => clickForm(e)}
+                >
+                    <label className={styles.label} id="1">
+                        <input type="radio" className={styles.input} value={1} {...register("name1")} />
+                        <span>Юный инженер</span>
+                    </label>
+                    <label className={styles.label} id="2">
+                        <input type="radio" className={styles.input} value={2} {...register("name1")} />
+                        <span>Молодой специалист</span>
+                    </label>
+                    <label className={styles.label} id="3">
+                        <input type="radio" className={styles.input} value={3} {...register("name1")} />
+                        <span>Начинающий эксперт</span>
+                    </label>
+                    <label className={styles.label} id="4">
+                        <input type="radio" className={styles.input} value={4} {...register("name1")} />
+                        <span>Будущий профессионал</span>
+                    </label>
 
-  return (
-    <>
-                        <div className={styles.taskInfo}>
-                        {checkClick && <OpacityTask/>}
-                        <h4 className={"task__subtitle " + (checkClick ? "answer" : "")}>Подчеркни верный ответ</h4>
-                        <form className={styles.form} onClick={
-                            (e) => clickForm(e)} 
-                            >
-                            <label className={styles.label} 
-                            onMouseDown={(e) => mouseStart(e)}
-                            onMouseMove={() => mouseMove()}
-                            onMouseLeave={() => mouseUp()}
-                            onTouchStart={(e) => dragStart(e)} 
-                            onTouchMove = {() => dragMove()}
-                            onTouchEnd={() => dragEnd()}
-                             id="1">
-                                <input type="radio" className={styles.input} value={1} {...register("name1")}/>
-                                <span>Юный инженер</span>
-                            </label>
-                            <label className={styles.label}
-                                                        onMouseDown={(e) => mouseStart(e)}
-                                                        onMouseMove={() => mouseMove()}
-                                                        onMouseLeave={() => mouseUp()}
-                                                        onTouchStart={(e) => dragStart(e)} 
-                                                        onTouchMove = {() => dragMove()}
-                                                        onTouchEnd={() => dragEnd()}
-                            
-                            id="2">
-                                <input type="radio" className={styles.input} value={2} {...register("name1")}/>
-                                <span>Молодой специалист</span>
-                            </label>
-                            <label className={styles.label}
-                                                        onMouseDown={(e) => mouseStart(e)}
-                                                        onMouseMove={() => mouseMove()}
-                                                        onMouseLeave={() => mouseUp()}
-                                                        onTouchStart={(e) => dragStart(e)} 
-                                                        onTouchMove = {() => dragMove()}
-                                                        onTouchEnd={() => dragEnd()}id="3">
-                                <input type="radio" className={styles.input} value={3} {...register("name1")}/>
-                                <span>Начинающий эксперт</span>
-                            </label>
-                            <label className={styles.label} id="4"
-                                                        onMouseDown={(e) => mouseStart(e)}
-                                                        onMouseMove={() => mouseMove()}
-                                                        onMouseLeave={() => mouseUp()}
-                                                        onTouchStart={(e) => dragStart(e)} 
-                                                        onTouchMove = {() => dragMove()}
-                                                        onTouchEnd={() => dragEnd()}>
-                                <input type="radio" className={styles.input} value={4} {...register("name1")}/>
-                                <span>Будущий профессионал</span>
-                            </label>
-
-                        </form>
+                </form>
 
 
-                </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 export default Task32;
